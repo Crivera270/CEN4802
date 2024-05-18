81ec798de693ee5bbe965cc8d255af6a87c3fee9
Christina <Christinarivera243@gmail.com>
2024-05-18 09:38:52 -0400

    first commit

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -3,21 +3,10 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Fibonacci {
-    /**
-     * Christina Rivera 05/18/2024
-     * Fibonacci
-     * This class contains the methods to create an application that outputs
-     * the Fibonacci number of an input.
-     */
+  
     static int Fib(int n)
     {
-        /**
-         * Fib
-         * This method contains the formula used to obtain the Fibonacci number
-         * of an input
-         * @param the inputed integer
-         * @return the fibonacci number of the input
-         */
+
         if (n == 0 || n == 1)
             return n;
 
@@ -25,11 +14,7 @@ public class Fibonacci {
 
     }
     public static void main (String[] args) {
-        /**
-         * main
-         * This method contains the application the user will interact with to input
-         * an integer and get the fibonacci number in return.
-         */
+
         System.out.println("Fibonacci Sequence:");
         System.out.println("Please enter value below");
 
