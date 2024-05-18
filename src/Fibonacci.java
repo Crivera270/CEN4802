07323d2e174480e79afc2694c52dafaac89a8914
Christina <Christinarivera243@gmail.com>
2024-05-18 09:56:31 -0400

    comments

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -3,17 +3,36 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Fibonacci {
-  
-    static int Fib(int n)
-    {
+    /**
+     * Christina Rivera 05/18/2024
+     * Fibonacci
+     * This class contains the methods to create an application that outputs
+     * the Fibonacci number of an input.
+     */
+
+    static int Fib(int n) {
+        /**
+         * Fib
+         * This method contains the formula used to obtain the Fibonacci number
+         * of an input
+         * @param the inputed integer
+         * @return the fibonacci number of the input
+         */
+
 
         if (n == 0 || n == 1)
             return n;
 
-        return Fib(n- 1) + Fib(n - 2);
+        return Fib(n - 1) + Fib(n - 2);
 
     }
-    public static void main (String[] args) {
+
+    public static void main(String[] args) {
+        /**
+        * main
+        * This method contains the application the user will interact with to input
+        * an integer and get the fibonacci number in return.
+        */
 
         System.out.println("Fibonacci Sequence:");
         System.out.println("Please enter value below");
@@ -21,14 +40,13 @@ public class Fibonacci {
         Scanner value = new Scanner(System.in);
         int number = value.nextInt();
 
-        System.out.println("You entered: "+ number);
+        System.out.println("You entered: " + number);
 
         System.out.println("Processing...");
-        System.out.println("Fibonacci of "+number+" is: "+Fib(number));
+        System.out.println("Fibonacci of " + number + " is: " + Fib(number));
 
 
     }
-
 
 
 }
